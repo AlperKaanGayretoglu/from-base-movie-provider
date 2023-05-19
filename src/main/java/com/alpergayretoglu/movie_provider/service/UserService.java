c6896a0463520746cc137812cfab6c65a324e077
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-05-19 12:05:19 +0300

    Created all the Services, NOTE: AuthenticationService has not been changed! If you deem necessary, you can change it in the future.

--- src/main/java/com/alpergayretoglu/movie_provider/service/UserService.java
+++ src/main/java/com/alpergayretoglu/movie_provider/service/UserService.java
@@ -1,8 +1,9 @@
 package com.alpergayretoglu.movie_provider.service;
 
+import com.alpergayretoglu.movie_provider.exception.BadRequestException;
 import com.alpergayretoglu.movie_provider.exception.BusinessException;
 import com.alpergayretoglu.movie_provider.exception.ErrorCode;
-import com.alpergayretoglu.movie_provider.model.entity.User;
+import com.alpergayretoglu.movie_provider.model.entity.*;
 import com.alpergayretoglu.movie_provider.model.enums.UserRole;
 import com.alpergayretoglu.movie_provider.model.request.user.CreateUserRequest;
 import com.alpergayretoglu.movie_provider.model.request.user.UpdateUserRequest;
@@ -14,6 +15,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -21,8 +24,13 @@ import javax.transaction.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final MovieService movieService;
+    private final CategoryService categoryService;
+    private final SubscriptionService subscriptionService;
+    private final ContractRecordService contractRecordService;
     private final PasswordEncoder passwordEncoder;
 
+    // TODO: DEFAULT METHODS
     public Page<User> listUsers(Pageable pageable) {
         return userRepository.findAll(pageable);
     }
@@ -58,6 +66,88 @@ public class UserService {
 
         userRepository.delete(user);
 
+        return user;
+    }
+
+    // TODO: EXTRA METHODS
+
+    public ContractRecord subscribe(String userId, String subscriptionId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.RESOURCE_MISSING, "User not found"));
+        Subscription subscription = subscriptionService.findById(subscriptionId);
+
+        // check if user is verified
+        if (!user.isVerified()) throw new BadRequestException("only verified users can subscribe");
+
+        // check if user has already a subscription (allow only upgrading)
+        ContractRecord contractRecord = user.getContractRecord();
+
+        if (contractRecord != null) {
+            if (contractRecord.getDuration() >= subscription.getDuration()) {
+                throw new BadRequestException("you can only upgrade your subscription");
+            }
+        }
+
+        // when a guest user buys a subscription, assign a member role
+        if (user.getUserRole() == UserRole.GUEST) user.setUserRole(UserRole.MEMBER);
+
+        if (contractRecord == null) return contractRecordService.addContract(user, subscription);
+        else return contractRecordService.updateContract(contractRecord, subscription);
+    }
+
+    // Interests : Follow Categories and Favorite Movies
+    public User favoriteMovie(String userId, String movieId) {
+        return addOrRemoveMovieFromUserFavoriteMovies(userId, movieId, true);
+    }
+
+    public User unfavoriteMovie(String userId, String movieId) {
+        return addOrRemoveMovieFromUserFavoriteMovies(userId, movieId, false);
+    }
+
+    public User followCategory(String userId, String categoryId) {
+        return followHelper(userId, categoryId, true);
+    }
+
+    public User unfollowCategory(String userId, String categoryId) {
+        return followHelper(userId, categoryId, false);
+    }
+
+    /**
+     * Helper method to avoid duplicate codes.
+     * boolean isAddition field checks if it is a favorite or unfavorite method
+     */
+    private User addOrRemoveMovieFromUserFavoriteMovies(String userId, String movieId, boolean isAddition) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.RESOURCE_MISSING, "User not found"));
+        Movie movie = movieService.findMovieById(movieId);
+        Set<Movie> movies = user.getFavoriteMovies();
+
+        if (isAddition) {
+            movies.add(movie);
+        } else {
+            movies.remove(movie);
+        }
+
+        userRepository.save(user);
+        return user;
+    }
+
+    /**
+     * Helper method to avoid duplicate codes. <br>
+     * (TODO still needs a refactor of duplicate codes with addOrRemoveMovieFromUserFavoriteMovies method) <br>
+     * To refactor, maybe use ICrudService interface? and give the method of class and its service. <br>
+     * boolean isFollow field checks if it is a follow or unfollow request
+     */
+    private User followHelper(String userId, String categoryId, boolean isFollow) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.RESOURCE_MISSING, "User not found"));
+        Category category = categoryService.findCategoryById(categoryId);
+
+        List<Category> categories = user.getFollowedCategories();
+
+        if (isFollow && !categories.contains(category)) categories.add(category);
+        else categories.remove(category);
+        userRepository.save(user);
         return user;
     }
 
