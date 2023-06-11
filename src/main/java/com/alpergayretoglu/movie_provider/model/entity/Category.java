d9cb78dd3686b24c19369a98cc08f2479233c62f
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-06-11 13:45:49 +0300

    [UPDATE] Category Logic

--- src/main/java/com/alpergayretoglu/movie_provider/model/entity/Category.java
+++ src/main/java/com/alpergayretoglu/movie_provider/model/entity/Category.java
@@ -1,6 +1,5 @@
 package com.alpergayretoglu.movie_provider.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import javax.persistence.*;
@@ -18,27 +17,20 @@ public class Category extends BaseEntity {
     @Column(unique = true)
     private String name;
 
-    // TODO: Do we need to specify with which table ???
-    @OneToOne // TODO: Is this ManyToOne OR OneToOne ???
-    private Category parent;
-
-    // TODO: Do we need to specify with which table ???
-    /*
-    @JoinTable(name = "category_movies", // TODO: Change the name to -> movie_categories ?
-            joinColumns = @JoinColumn(name = "categories_id"),
-            inverseJoinColumns = @JoinColumn(name = "movie_id")
-    )
-    */
     @ManyToMany(fetch = FetchType.LAZY)
-    // @ManyToMany(mappedBy = "categories", fetch = FetchType.LAZY) // TODO: Is this correct ???
-    @JsonIgnoreProperties("categories") // TODO: Is this necessary ???
     @Builder.Default
     private Set<Movie> movies = new HashSet<>();
 
-    // TODO: Implement this
-//    @ManyToMany(fetch = FetchType.LAZY)
-//    private Set<User> followers;
+    @ManyToMany(mappedBy = "followedCategories", fetch = FetchType.LAZY)
+    private Set<User> followers;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Category parent;
 
     private boolean isSuperCategory;
+
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY)
+    @Builder.Default
+    private Set<Category> subCategories = new HashSet<>();
 
 }
